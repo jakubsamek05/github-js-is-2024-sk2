bef71cf5d013dfeb16d462bfac50e2cd777811f2
Jakub Samek <jakub.samek@tul.cz>
2024-11-06 22:47:57 +0100

    #3 Ukol - Obdelniky

---
+++ IS-Projekty/program003e-ukol/Program.cs
@@ -0,0 +1,45 @@
+﻿        string again = "a";
+        while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("****************************");
+            Console.WriteLine("******** Obdelníky ***********");
+            Console.WriteLine("****************************");
+            Console.WriteLine();
+
+            int size;
+            Console.Write("Zadejte velikost obdelníku (celé kladné číslo): ");
+            while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+            {
+                Console.Write("Nezadali jste platné celé kladné číslo. Zadejte velikost znovu: ");
+                Console.WriteLine();
+            }
+
+            int rows = 4;
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write(new string('\t', 4)); 
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine(); 
+            }
+
+            
+            Console.WriteLine(); 
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+            again = Console.ReadLine();
+        }
